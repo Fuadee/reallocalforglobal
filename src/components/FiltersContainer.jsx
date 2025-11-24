db640c93121cd5a19850fddace66147a1a2cc86d
Fuadee <fuadee.gallantheart@gmail.com>
2025-11-24 11:10:40 +0700

    Improve mobile filter taps and star selection

---
+++ src/components/FiltersContainer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import GroupFilters from './GroupFilters';
+import ScoreFilter from './ScoreFilter';
+import SpecialFilters from './SpecialFilters';
+import './FilterBar.css';
+
+function FiltersContainer({
+  groups,
+  selectedGroup,
+  onSelectGroup,
+  specialTags,
+  selectedTags,
+  onToggleTag,
+  selectedStars,
+  onToggleStar,
+}) {
+  return (
+    <div className="filters-container" aria-label="Krabi map filters">
+      <div className="filters-surface">
+        <GroupFilters groups={groups} selectedGroup={selectedGroup} onSelect={onSelectGroup} />
+        <ScoreFilter selectedStars={selectedStars} onToggleStar={onToggleStar} />
+        <SpecialFilters
+          specialTags={specialTags}
+          selectedTags={selectedTags}
+          onToggleTag={onToggleTag}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default FiltersContainer;
